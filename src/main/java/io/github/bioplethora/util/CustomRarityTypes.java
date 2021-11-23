0a8e72088dddc9c01ab85f08ea3a97c5c300aafc
Sajevius <sajeviuscrend@gmail.com>
2021-11-23 11:08:55 +0800

    Created working custom rarity types & deleted BioplethoraRarity enum

---
+++ src/main/java/io/github/bioplethora/util/CustomRarityTypes.java
@@ -0,0 +1,9 @@
+package io.github.bioplethora.util;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.util.text.TextFormatting;
+
+public class CustomRarityTypes {
+    public static final Rarity GODLY = Rarity.create("godly", TextFormatting.GOLD);
+    public static final Rarity LEGENDARY = Rarity.create("legendary", TextFormatting.RED);
+}
