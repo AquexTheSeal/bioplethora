c97e63c54379e7e3751146f8dfea00250e807244
Aqu <sheenix15@gmail.com>
2021-11-25 20:55:41 +0800

    More Peaguin stuff.

--- src/main/java/io/github/bioplethora/client/entity/model/PeaguinEntityModel.java
+++ src/main/java/io/github/bioplethora/client/entity/model/PeaguinEntityModel.java
@@ -2,10 +2,14 @@ package io.github.bioplethora.client.entity.model;
 
 import io.github.bioplethora.Bioplethora;
 import io.github.bioplethora.entity.AlphemEntity;
+import io.github.bioplethora.entity.CrephoxlEntity;
 import io.github.bioplethora.entity.PeaguinEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class PeaguinEntityModel extends AnimatedGeoModel<PeaguinEntity> {
 
@@ -35,4 +39,14 @@ public class PeaguinEntityModel extends AnimatedGeoModel<PeaguinEntity> {
             return new ResourceLocation(Bioplethora.MOD_ID, "animations/peaguin.animation.json");
         }
     }
+
+    /*@Override
+    public void setLivingAnimations(PeaguinEntity entity, Integer uniqueID, @SuppressWarnings("rawtypes") AnimationEvent customPredicate) {
+        super.setLivingAnimations(entity, uniqueID, customPredicate);
+
+        IBone head = this.getAnimationProcessor().getBone("head");
+        EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+        head.setRotationX((extraData.headPitch) * ((float) Math.PI / 180F));
+        head.setRotationY((extraData.netHeadYaw) * ((float) Math.PI / 270F));
+    }*/
 }
--- src/main/java/io/github/bioplethora/entity/PeaguinEntity.java
+++ src/main/java/io/github/bioplethora/entity/PeaguinEntity.java
@@ -4,28 +4,35 @@ import io.github.bioplethora.config.BioplethoraConfig;
 import io.github.bioplethora.entity.ai.AnimalAnimatableMeleeGoal;
 import io.github.bioplethora.entity.ai.AnimalAnimatableMoveToTargetGoal;
 import io.github.bioplethora.registry.BioplethoraEntities;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.controller.DolphinLookController;
 import net.minecraft.entity.ai.controller.MovementController;
 import net.minecraft.entity.ai.goal.*;
-import net.minecraft.entity.passive.SquidEntity;
-import net.minecraft.entity.passive.WaterMobEntity;
-import net.minecraft.entity.passive.WolfEntity;
+import net.minecraft.entity.monster.AbstractSkeletonEntity;
+import net.minecraft.entity.passive.*;
+import net.minecraft.entity.passive.fish.AbstractFishEntity;
+import net.minecraft.entity.passive.horse.LlamaEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.item.*;
 import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.pathfinding.PathNavigator;
-import net.minecraft.pathfinding.PathNodeType;
-import net.minecraft.pathfinding.SwimmerPathNavigator;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.pathfinding.*;
 import net.minecraft.tags.FluidTags;
-import net.minecraft.util.DamageSource;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.registries.ForgeRegistries;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -34,15 +41,22 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-public class PeaguinEntity extends AnimatableAnimalEntity implements IAnimatable {
+import javax.annotation.Nullable;
+import java.util.UUID;
+
+public class PeaguinEntity extends AnimatableAnimalEntity implements IAnimatable, IAngerable {
+
+    private static final DataParameter<Integer> DATA_REMAINING_ANGER_TIME = EntityDataManager.defineId(WolfEntity.class, DataSerializers.INT);
+    private static final RangedInteger PERSISTENT_ANGER_TIME = TickRangeConverter.rangeOfSeconds(20, 39);
+    private UUID persistentAngerTarget;
+
     private final AnimationFactory factory = new AnimationFactory(this);
 
     public PeaguinEntity(EntityType<? extends AnimatableAnimalEntity> type, World world) {
         super(type, world);
         this.setPathfindingMalus(PathNodeType.WATER, 0.0F);
+        this.moveControl = new PeaguinEntity.MoveHelperController(this);
         this.noCulling = true;
-        /*this.moveControl = new MoveHelperController(this);*/
-        this.lookControl = new DolphinLookController(this, 10);
     }
 
     public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
@@ -59,21 +73,37 @@ public class PeaguinEntity extends AnimatableAnimalEntity implements IAnimatable
     @Override
     protected void registerGoals() {
         super.registerGoals();
-        this.goalSelector.addGoal(1, new PanicGoal(this, 1.2));
+        /*this.goalSelector.addGoal(3, new PanicGoal(this, 1.2));
         this.goalSelector.addGoal(3, new LookAtGoal(this, PlayerEntity.class, 24.0F));
         this.goalSelector.addGoal(4, new RandomWalkingGoal(this, 0.5F));
         this.goalSelector.addGoal(4, new LookRandomlyGoal(this));
-        this.goalSelector.addGoal(4, new RandomSwimmingGoal(this, 5, 40));
-        this.goalSelector.addGoal(2, new TemptGoal(this, 1, Ingredient.of(Items.SALMON), false));
-        this.goalSelector.addGoal(2, new TemptGoal(this, 1, Ingredient.of(Items.COOKED_SALMON), false));
-        this.goalSelector.addGoal(2, new TemptGoal(this, 1, Ingredient.of(Items.COD), false));
-        this.goalSelector.addGoal(2, new TemptGoal(this, 1, Ingredient.of(Items.COOKED_COD), false));
-        this.goalSelector.addGoal(3, new FollowParentGoal(this, 1));
-        this.goalSelector.addGoal(3, new FollowOwnerGoal(this, 1, 10, 2, false));
-        this.goalSelector.addGoal(6, new FollowMobGoal(this, (float) 1, 10, 5));
+        this.goalSelector.addGoal(4, new RandomSwimmingGoal(this, 2.5, 40));
 
+        this.goalSelector.addGoal(3, new TemptGoal(this, 1, Ingredient.of(Items.SALMON), false));
+        this.goalSelector.addGoal(3, new TemptGoal(this, 1, Ingredient.of(Items.COOKED_SALMON), false));
+        this.goalSelector.addGoal(3, new TemptGoal(this, 1, Ingredient.of(Items.COD), false));
+        this.goalSelector.addGoal(3, new TemptGoal(this, 1, Ingredient.of(Items.COOKED_COD), false));
+
+        this.goalSelector.addGoal(4, new FollowParentGoal(this, 1));
+        this.goalSelector.addGoal(2, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, false));
+
+        this.targetSelector.addGoal(2, new HurtByTargetGoal(this).setAlertOthers(this.getClass()));
         this.targetSelector.addGoal(1, new OwnerHurtByTargetGoal(this));
-        this.targetSelector.addGoal(1, new OwnerHurtTargetGoal(this));
+        this.targetSelector.addGoal(2, new OwnerHurtTargetGoal(this));*/
+
+        this.goalSelector.addGoal(1, new SwimGoal(this));
+        this.goalSelector.addGoal(5, new AnimalAnimatableMoveToTargetGoal(this, 1.6, 8));
+        this.goalSelector.addGoal(5, new AnimalAnimatableMeleeGoal(this, 20, 0.23, 0.47));
+        this.goalSelector.addGoal(6, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, false));
+        this.goalSelector.addGoal(7, new BreedGoal(this, 1.0D));
+        this.goalSelector.addGoal(8, new PeaguinEntity.SwimGoal(this));
+        this.goalSelector.addGoal(10, new LookAtGoal(this, PlayerEntity.class, 8.0F));
+        this.goalSelector.addGoal(10, new LookRandomlyGoal(this));
+        this.targetSelector.addGoal(1, new OwnerHurtByTargetGoal(this));
+        this.targetSelector.addGoal(2, new OwnerHurtTargetGoal(this));
+        this.targetSelector.addGoal(3, (new HurtByTargetGoal(this)).setAlertOthers());
+        this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, 10, true, false, this::isAngryAt));
+        this.targetSelector.addGoal(8, new ResetAngerGoal<>(this, true));
     }
 
     @Override
@@ -86,6 +116,27 @@ public class PeaguinEntity extends AnimatableAnimalEntity implements IAnimatable
         return this.factory;
     }
 
+    @Override
+    public net.minecraft.util.SoundEvent getAmbientSound() {
+        return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.dolphin.ambient"));
+    }
+
+    @Override
+    public net.minecraft.util.SoundEvent getHurtSound(DamageSource damageSource) {
+        return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.dolphin.hurt"));
+    }
+
+    @Override
+    public net.minecraft.util.SoundEvent getDeathSound() {
+        return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.dolphin.death"));
+    }
+
+    @Override
+    public void playStepSound(BlockPos pos, BlockState blockIn) {
+        this.playSound((net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.salmon.flop")),
+                0.15f, 1);
+    }
+
     private <E extends IAnimatable>PlayState predicate(AnimationEvent<E> event) {
 
         if(this.isDeadOrDying() || this.dead) {
@@ -93,8 +144,13 @@ public class PeaguinEntity extends AnimatableAnimalEntity implements IAnimatable
             return PlayState.CONTINUE;
         }
 
-        if(this.getAttacking()) {
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.peaguin.death", true));
+        if(this.getAttacking() && this.shouldDropLoot()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.peaguin.attacking", true));
+            return PlayState.CONTINUE;
+        }
+
+        if(this.isSwimming() || this.isInWaterOrBubble() || this.isInWater()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.peaguin.swimming", true));
             return PlayState.CONTINUE;
         }
 
@@ -115,32 +171,102 @@ public class PeaguinEntity extends AnimatableAnimalEntity implements IAnimatable
         return this.isBaby() ? entitySize.height * 0.8F : 1.0F;
     }
 
-    public void aiStep() {
+    /*public void aiStep() {
         super.aiStep();
-        if (!this.isBaby()) {
+        if (this.shouldDropLoot() && !this.isTame()) {
+            this.goalSelector.removeGoal(new PanicGoal(this, 1.2));
             this.goalSelector.addGoal(1, new BreedGoal(this, 1));
             this.goalSelector.addGoal(2, new AnimalAnimatableMoveToTargetGoal(this, 1.6, 8));
-            this.goalSelector.addGoal(2, new AnimalAnimatableMeleeGoal(this, 40, 0.2, 0.3));
+            this.goalSelector.addGoal(2, new AnimalAnimatableMeleeGoal(this, 20, 0.23, 0.47));
+        } else if (this.shouldDropLoot() && this.isTame()) {
+            this.goalSelector.removeGoal(new PanicGoal(this, 1.2));
+            this.goalSelector.addGoal(1, new BreedGoal(this, 1));
+            this.goalSelector.addGoal(2, new AnimalAnimatableMoveToTargetGoal(this, 1.6, 8));
+            this.goalSelector.addGoal(2, new AnimalAnimatableMeleeGoal(this, 20, 0.23, 0.47));
         }
+    }*/
 
-        if (this.isTame()) {
-            this.targetSelector.removeGoal(new HurtByTargetGoal(this).setAlertOthers(this.getClass()));
+    public boolean isFood(ItemStack p_70877_1_) {
+        Item item = p_70877_1_.getItem();
+        return item.isEdible() && item.getFoodProperties().isMeat();
+    }
+
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(DATA_REMAINING_ANGER_TIME, 0);
+    }
+
+    public ActionResultType mobInteract(PlayerEntity p_230254_1_, Hand p_230254_2_) {
+        ItemStack itemstack = p_230254_1_.getItemInHand(p_230254_2_);
+        Item item = itemstack.getItem();
+        if (this.level.isClientSide) {
+            boolean flag = this.isOwnedBy(p_230254_1_) || this.isTame() || item.isEdible() && item.getFoodProperties().isMeat() && !this.isTame() && !this.isAngry();
+            return flag ? ActionResultType.CONSUME : ActionResultType.PASS;
+        } else {
+            if (this.isTame()) {
+                if (this.isFood(itemstack) && this.getHealth() < this.getMaxHealth()) {
+                    if (!p_230254_1_.abilities.instabuild) {
+                        itemstack.shrink(1);
+                    }
+
+                    this.heal((float)item.getFoodProperties().getNutrition());
+                    return ActionResultType.SUCCESS;
+                }
+
+            } else if (item == ItemTags.getAllTags().getTagOrEmpty(new ResourceLocation("minecraft:fishes")) && !this.isAngry()) {
+                if (!p_230254_1_.abilities.instabuild) {
+                    itemstack.shrink(1);
+                }
+
+                if (this.random.nextInt(3) == 0 && !net.minecraftforge.event.ForgeEventFactory.onAnimalTame(this, p_230254_1_)) {
+                    this.tame(p_230254_1_);
+                    this.navigation.stop();
+                    this.setTarget((LivingEntity)null);
+                    this.setOrderedToSit(true);
+                    this.level.broadcastEntityEvent(this, (byte)7);
+                } else {
+                    this.level.broadcastEntityEvent(this, (byte)6);
+                }
+
+                return ActionResultType.SUCCESS;
+            }
+
+            return super.mobInteract(p_230254_1_, p_230254_2_);
         }
     }
 
-    @Override
-    public boolean isFood(ItemStack stack) {
-        if (stack == null)
-            return false;
-        if (Items.SALMON == stack.getItem())
-            return true;
-        if (Items.COOKED_SALMON == stack.getItem())
-            return true;
-        if (Items.COD == stack.getItem())
-            return true;
-        if (Items.COOKED_COD == stack.getItem())
-            return true;
-        return false;
+    public int getRemainingPersistentAngerTime() {
+        return this.entityData.get(DATA_REMAINING_ANGER_TIME);
+    }
+
+    public void setRemainingPersistentAngerTime(int p_230260_1_) {
+        this.entityData.set(DATA_REMAINING_ANGER_TIME, p_230260_1_);
+    }
+
+    public void startPersistentAngerTimer() {
+        this.setRemainingPersistentAngerTime(PERSISTENT_ANGER_TIME.randomValue(this.random));
+    }
+
+    @Nullable
+    public UUID getPersistentAngerTarget() {
+        return this.persistentAngerTarget;
+    }
+
+    public void setPersistentAngerTarget(@Nullable UUID p_230259_1_) {
+        this.persistentAngerTarget = p_230259_1_;
+    }
+
+    public void addAdditionalSaveData(CompoundNBT p_213281_1_) {
+        super.addAdditionalSaveData(p_213281_1_);
+
+        this.addPersistentAngerSaveData(p_213281_1_);
+    }
+
+    public void readAdditionalSaveData(CompoundNBT p_70037_1_) {
+        super.readAdditionalSaveData(p_70037_1_);
+
+        if(!level.isClientSide) //FORGE: allow this entity to be read from nbt on client. (Fixes MC-189565)
+            this.readPersistentAngerSaveData((ServerWorld)this.level, p_70037_1_);
     }
 
     public boolean canBreatheUnderwater() {
@@ -171,60 +297,64 @@ public class PeaguinEntity extends AnimatableAnimalEntity implements IAnimatable
         return true;
     }
 
-    /*@Override
-    protected PathNavigator createNavigation(World worldIn) {
-        return new SwimmerPathNavigator(this, worldIn);
+    protected boolean canRandomSwim() {
+        return true;
+    }
+
+    static class SwimGoal extends RandomSwimmingGoal {
+        private final PeaguinEntity fish;
+
+        public SwimGoal(PeaguinEntity p_i48856_1_) {
+            super(p_i48856_1_, 1.0D, 40);
+            this.fish = p_i48856_1_;
+        }
+
+        public boolean canUse() {
+            return this.fish.canRandomSwim() && super.canUse();
+        }
+    }
+
+    protected PathNavigator createNavigation(World p_175447_1_) {
+        if (this.isInWater()) {
+            return new SwimmerPathNavigator(this, p_175447_1_);
+        } else {
+            return new GroundPathNavigator(this, p_175447_1_);
+        }
     }
 
     static class MoveHelperController extends MovementController {
-        private final PeaguinEntity whale;
+        private final PeaguinEntity fish;
 
-        MoveHelperController(PeaguinEntity whale) {
-            super(whale);
-            this.whale = whale;
+        MoveHelperController(PeaguinEntity p_i48857_1_) {
+            super(p_i48857_1_);
+            this.fish = p_i48857_1_;
         }
 
-        @Override
         public void tick() {
-            if (this.whale.isEyeInFluid(FluidTags.WATER)) {
-                this.whale.setDeltaMovement(this.whale.getDeltaMovement().add(0.0D, 0.005D, 0.0D));
+            if (this.fish.isEyeInFluid(FluidTags.WATER)) {
+                this.fish.setDeltaMovement(this.fish.getDeltaMovement().add(0.0D, 0.005D, 0.0D));
             }
 
-            if (this.operation == MovementController.Action.MOVE_TO && !this.whale.getNavigation().isDone()) {
-                double d0 = this.wantedX - this.whale.getX();
-                double d1 = this.wantedY - this.whale.getY();
-                double d2 = this.wantedZ - this.whale.getZ();
-                double d3 = d0 * d0 + d1 * d1 + d2 * d2;
-
-                if (d3 < (double)2.5000003E-7F) {
-                    this.mob.setZza(0.0F);
-                } else {
-                    float f = (float)(MathHelper.atan2(d2, d0) * (double)(180F / (float)Math.PI)) - 90.0F;
-                    this.whale.yRot = this.rotlerp(this.whale.yRot, f, 10.0F);
-                    this.whale.yBodyRot = this.whale.yRot;
-                    this.whale.yHeadRot = this.whale.yRot;
-                    float f1 = (float)(this.speedModifier * this.whale.getAttributeValue(Attributes.MOVEMENT_SPEED));
-
-                    if (this.whale.isInWater()) {
-                        this.whale.setSpeed(f1 * 0.02F);
-                        float f2 = -((float)(MathHelper.atan2(d1, (double)MathHelper.sqrt(d0 * d0 + d2 * d2)) * (double)(180F / (float)Math.PI)));
-                        f2 = MathHelper.clamp(MathHelper.wrapDegrees(f2), -85.0F, 85.0F);
-                        this.whale.xRot = this.rotlerp(this.whale.xRot, f2, 5.0F);
-                        float f3 = MathHelper.cos(this.whale.xRot * ((float)Math.PI / 180F));
-                        float f4 = MathHelper.sin(this.whale.xRot * ((float)Math.PI / 180F));
-                        this.whale.zza = f3 * f1;
-                        this.whale.yya = -f4 * f1;
-                    } else {
-                        this.whale.setSpeed(f1 * 0.1F);
-                    }
-
+            if (this.operation == MovementController.Action.MOVE_TO && !this.fish.getNavigation().isDone()) {
+                float f = (float)(this.speedModifier * this.fish.getAttributeValue(Attributes.MOVEMENT_SPEED));
+                this.fish.setSpeed(MathHelper.lerp(0.125F, this.fish.getSpeed(), f));
+                double d0 = this.wantedX - this.fish.getX();
+                double d1 = this.wantedY - this.fish.getY();
+                double d2 = this.wantedZ - this.fish.getZ();
+                if (d1 != 0.0D) {
+                    double d3 = (double)MathHelper.sqrt(d0 * d0 + d1 * d1 + d2 * d2);
+                    this.fish.setDeltaMovement(this.fish.getDeltaMovement().add(0.0D, (double)this.fish.getSpeed() * (d1 / d3) * 0.1D, 0.0D));
                 }
+
+                if (d0 != 0.0D || d2 != 0.0D) {
+                    float f1 = (float)(MathHelper.atan2(d2, d0) * (double)(180F / (float)Math.PI)) - 90.0F;
+                    this.fish.yRot = this.rotlerp(this.fish.yRot, f1, 90.0F);
+                    this.fish.yBodyRot = this.fish.yRot;
+                }
+
             } else {
-                this.whale.setSpeed(0.0F);
-                this.whale.setXxa(0.0F);
-                this.whale.setYya(0.0F);
-                this.whale.setZza(0.0F);
+                this.fish.setSpeed(0.0F);
             }
         }
-    }*/
+    }
 }
