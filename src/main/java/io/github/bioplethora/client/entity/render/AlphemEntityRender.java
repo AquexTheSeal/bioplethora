4e3088d48096099a9e1fa37e24b1df2c38606d79
Aqu <sheenix15@gmail.com>
2021-11-29 21:30:12 +0800

    Made Alphems Biped

--- src/main/java/io/github/bioplethora/client/entity/render/AlphemEntityRender.java
+++ src/main/java/io/github/bioplethora/client/entity/render/AlphemEntityRender.java
@@ -10,8 +10,6 @@ import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.vector.Vector3f;
 import software.bernie.geckolib3.geo.render.built.GeoBone;
@@ -41,6 +39,46 @@ public class AlphemEntityRender extends GeoEntityRenderer<AlphemEntity> {
 
     @Override
     public void renderRecursively(GeoBone bone, MatrixStack stack, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
+        if (bone.getName().equals("right_hand") || bone.getName().equals("right_arm")) {
+            if (!bone.isHidden) {
+                stack.pushPose();
+
+                //position / translation
+                stack.translate(0.4D, 0.25D, -0.15D);
+
+                //rotation
+                stack.mulPose(Vector3f.XP.rotationDegrees(-75));
+                stack.mulPose(Vector3f.YP.rotationDegrees(-15));
+                stack.mulPose(Vector3f.ZP.rotationDegrees(0));
+
+                //size / scale
+                stack.scale(1.0f, 1.0f, 1.0f);
+
+                Minecraft.getInstance().getItemRenderer().renderStatic(this.mainHand.getStack(), ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND, packedLightIn, packedOverlayIn, stack, this.rtb);
+                stack.popPose();
+                bufferIn = rtb.getBuffer(RenderType.entitySmoothCutout(whTexture));
+            }
+        }
+        if (bone.getName().equals("left_hand") || bone.getName().equals("left_arm")) {
+            if (!bone.isHidden) {
+                stack.pushPose();
+
+                //position / translation
+                stack.translate(-0.4D, 0.25D, -0.25D);
+
+                //rotation
+                stack.mulPose(Vector3f.XP.rotationDegrees(-75));
+                stack.mulPose(Vector3f.YP.rotationDegrees(15));
+                stack.mulPose(Vector3f.ZP.rotationDegrees(0));
+
+                //size / scale
+                stack.scale(1.0f, 1.0f, 1.0f);
+
+                Minecraft.getInstance().getItemRenderer().renderStatic(this.offHand.getStack(), ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND, packedLightIn, packedOverlayIn, stack, this.rtb);
+                stack.popPose();
+                bufferIn = rtb.getBuffer(RenderType.entitySmoothCutout(whTexture));
+            }
+        }
         super.renderRecursively(bone, stack, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
     }
 
--- src/main/java/io/github/bioplethora/entity/AlphemEntity.java
+++ src/main/java/io/github/bioplethora/entity/AlphemEntity.java
@@ -3,17 +3,30 @@ package io.github.bioplethora.entity;
 import io.github.bioplethora.config.BioplethoraConfig;
 import io.github.bioplethora.entity.ai.MonsterAnimatableMeleeGoal;
 import io.github.bioplethora.entity.ai.MonsterAnimatableMoveToTargetGoal;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ILivingEntityData;
 import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.monster.SkeletonEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.IServerWorld;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.structure.StructureManager;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.registries.ForgeRegistries;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -22,6 +35,10 @@ import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
+
+import javax.annotation.Nullable;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
 
 public class AlphemEntity extends AnimatableMonsterEntity implements IAnimatable {
 
@@ -86,6 +103,37 @@ public class AlphemEntity extends AnimatableMonsterEntity implements IAnimatable
     @Override
     public AnimationFactory getFactory() {
         return this.factory;
+    }
+
+    @Nullable
+    public ILivingEntityData finalizeSpawn(IServerWorld iServerWorld, DifficultyInstance difficultyInstance, SpawnReason spawnReason, @Nullable ILivingEntityData iLivingEntityData, @Nullable CompoundNBT compoundNBT) {
+        iLivingEntityData = super.finalizeSpawn(iServerWorld, difficultyInstance, spawnReason, iLivingEntityData, compoundNBT);
+        if (BioplethoraConfig.COMMON.hellMode.get()) {
+            if (Math.random() < 0.5) {
+                if (!this.level.isClientSide())
+                    if (Math.random() < 0.5) {
+                        this.setItemSlot(EquipmentSlotType.OFFHAND, new ItemStack(Items.TOTEM_OF_UNDYING));
+                    }
+                    this.setItemSlot(EquipmentSlotType.MAINHAND, new ItemStack(Items.DIAMOND_SWORD));
+            } else {
+
+                if (!this.level.isClientSide())
+                    if (Math.random() < 0.5) {
+                        this.setItemSlot(EquipmentSlotType.OFFHAND, new ItemStack(Items.TOTEM_OF_UNDYING));
+                    }
+                    this.setItemSlot(EquipmentSlotType.MAINHAND, new ItemStack(Items.IRON_AXE));
+            }
+        } else {
+            if (Math.random() < 0.5) {
+                if (!this.level.isClientSide())
+                    this.setItemSlot(EquipmentSlotType.MAINHAND, new ItemStack(Items.WOODEN_SWORD));
+            } else {
+                if (!this.level.isClientSide())
+                    this.setItemSlot(EquipmentSlotType.MAINHAND, new ItemStack(Items.WOODEN_AXE));
+            }
+        }
+
+        return iLivingEntityData;
     }
 
     @Override
