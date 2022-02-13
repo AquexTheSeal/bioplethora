e623a51bb6fe54706ee06d2943d38f3bf5ef3a11
Aqu <sheenix15@gmail.com>
2022-02-13 12:33:43 +0800

    Improved Item Model Data Generator

--- src/main/java/io/github/bioplethora/generators/BioItemModelProvider.java
+++ src/main/java/io/github/bioplethora/generators/BioItemModelProvider.java
@@ -6,7 +6,7 @@ import io.github.bioplethora.registry.BioplethoraItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.TieredItem;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
@@ -27,8 +27,8 @@ public class BioItemModelProvider extends ItemModelProvider {
      */
     @Override
     protected void registerModels() {
-        generateItems(BioplethoraItems.ITEMS.getEntries());
-        generateBlockItems(BioplethoraBlocks.BLOCK_ITEMS.getEntries());
+        addItems(BioplethoraItems.ITEMS.getEntries());
+        addBlockItems(BioplethoraBlocks.BLOCK_ITEMS.getEntries());
     }
 
     @Nonnull
@@ -37,27 +37,43 @@ public class BioItemModelProvider extends ItemModelProvider {
         return Bioplethora.MOD_NAME + " Item models";
     }
 
-    private void generateItems(final Collection<RegistryObject<Item>> items) {
-        final ModelFile parentGenerated = getExistingFile(mcLoc("item/generated"));
-        final ModelFile.ExistingModelFile parentHandheld = getExistingFile(mcLoc("item/handheld"));
+    public ModelFile.ExistingModelFile getMcLoc(String mcModel) {
+        return getExistingFile(mcLoc(mcModel));
+    }
+
+    private void addItems(final Collection<RegistryObject<Item>> items) {
+        ModelFile.ExistingModelFile generated = getMcLoc("item/generated");
+        ModelFile.ExistingModelFile handheld = getMcLoc("item/handheld");
 
         for (RegistryObject<Item> item : items) {
             String name = item.getId().getPath();
 
-            if (!existingFileHelper.exists(new ResourceLocation(Bioplethora.MOD_ID, "item/" + name), TEXTURE) || existingFileHelper.exists(new ResourceLocation(Bioplethora.MOD_ID, "item/" + name), MODEL))
+            // Checks if item texture exist and model texture doesn't exist
+            if (!existingFileHelper.exists(new ResourceLocation(Bioplethora.MOD_ID, "item/" + name), TEXTURE)
+                    || existingFileHelper.exists(new ResourceLocation(Bioplethora.MOD_ID, "item/" + name), MODEL))
                 continue;
 
             Bioplethora.LOGGER.info(item.getId());
-            getBuilder(item.getId().getPath()).parent(item.get().getMaxDamage(ItemStack.EMPTY) > 0 && !(item.get() instanceof ArmorItem) ? parentHandheld : parentGenerated).texture("layer0", ItemModelProvider.ITEM_FOLDER + "/" + name);
+
+            getBuilder(item.getId().getPath()).parent(
+                    //If Item is TieredItem and if item is NOT ArmorItem, minecraft/handheld model will be generated for that item.
+                            // Otherwise, minecraft/generated model will be generated for that item.
+                    (item.get() instanceof TieredItem) && !(item.get() instanceof ArmorItem) ? handheld : generated)
+                    .texture("layer0", ItemModelProvider.ITEM_FOLDER + "/" + name);
         }
     }
 
-    private void generateBlockItems(final Collection<RegistryObject<Item>> blockItems) {
+    private void addBlockItems(final Collection<RegistryObject<Item>> blockItems) {
         for (RegistryObject<Item> item : blockItems) {
             String name = item.getId().getPath();
-            if (!existingFileHelper.exists(new ResourceLocation(Bioplethora.MOD_ID, "block/" + name), MODEL) || existingFileHelper.exists(new ResourceLocation(Bioplethora.MOD_ID, "item/" + name), MODEL))
+
+            // Checks if item texture exist and model texture doesn't exist
+            if (!existingFileHelper.exists(new ResourceLocation(Bioplethora.MOD_ID, "block/" + name), MODEL)
+                    || existingFileHelper.exists(new ResourceLocation(Bioplethora.MOD_ID, "item/" + name), MODEL))
                 continue;
+
             Bioplethora.LOGGER.info(item.getId());
+
             withExistingParent(name, new ResourceLocation(Bioplethora.MOD_ID, "block/" + name));
         }
     }
