787bdc9e147f67de887b8bdf520224928a55381c
Sajevius <sajeviuscrend@gmail.com>
2022-02-20 21:29:35 +0800

    Improved Nandbric Shortsword range & accuracy

--- src/main/java/io/github/bioplethora/item/weapons/NandbricShortswordItem.java
+++ src/main/java/io/github/bioplethora/item/weapons/NandbricShortswordItem.java
@@ -10,6 +10,7 @@ import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.UseAction;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.*;
@@ -20,6 +21,7 @@ import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -76,30 +78,27 @@ public class NandbricShortswordItem extends SwordItem {
     public void onUsingTick(ItemStack stack, LivingEntity player, int count) {
         super.onUsingTick(stack, player, count);
 
-        double range = 12.0D;
-        double distance = range * range;
+        World world = player.level;
 
+        double range = 24.0D;
+        double distance = range * range;
         Vector3d vec = player.getEyePosition(1);
         Vector3d vec1 = player.getViewVector(1);
         Vector3d finalVec = vec.add(vec1.x * range, vec1.y * range, vec1.z * range);
 
         AxisAlignedBB aabb = player.getBoundingBox().expandTowards(vec1.scale(range)).inflate(4.0D, 4.0D, 4.0D);
-
         EntityRayTraceResult result = ProjectileHelper.getEntityHitResult(player, vec, finalVec, aabb, (filter) -> {
             return !filter.isSpectator() && filter.isPickable();
         }, distance);
 
-        if(result != null && result.getEntity() instanceof LivingEntity) {
+        if(result != null && result.getEntity() instanceof LivingEntity && result.getEntity() != player) {
             LivingEntity target = (LivingEntity)result.getEntity();
-
-            double distToTarget = target.distanceToSqr(player.getX(), player.getY(), player.getZ());
-            double attackReachSqr = (player.getBbWidth() * 1.6F * player.getBbWidth() * 1.6F + target.getBbWidth());
 
             double vecX = target.getX() - player.getX();
             double vecY = player.getDeltaMovement().y;
             double vecZ = target.getZ() - player.getZ();
             double vecM = Math.sqrt((Math.pow(vecX, 2)) + Math.pow(vecY, 2) + Math.pow(vecZ, 2));
-            float speedModifier = 2f;
+            float speedModifier = 2.5f;
 
             vecX = (vecX / vecM) * speedModifier;
             vecY = (vecY / vecM) * speedModifier;
@@ -107,21 +106,26 @@ public class NandbricShortswordItem extends SwordItem {
 
             player.setDeltaMovement(vecX, vecY, vecZ);
 
-            if (distToTarget <= attackReachSqr) {
-                World world = player.level;
+            AxisAlignedBB hitrange = player.getBoundingBox().inflate(2.8D, 2.8D, 2.8D);
+
+            if (hitrange.intersects(target.getBoundingBox())) {
+                double x = target.getX(), y = target.getY(), z = target.getZ();
+
                 if(!world.isClientSide) {
+                    ((ServerWorld)world).sendParticles(ParticleTypes.SNEEZE, x, y + (player.getBbHeight() / 2), z, 30, 1.2, 1.2, 1.2, 0);
                     world.playSound(null, new BlockPos(player.getX(), player.getY(), player.getZ()), SoundEvents.ZOMBIE_INFECT, SoundCategory.PLAYERS, 1, 1);
                     world.playSound(null, new BlockPos(player.getX(), player.getY(), player.getZ()), SoundEvents.PLAYER_ATTACK_SWEEP, SoundCategory.PLAYERS, 1, 1);
+                    target.hurt(DamageSource.mobAttack(player), 7.0F);
+                    target.addEffect(new EffectInstance(Effects.POISON, 100, 7));
+                    ((PlayerEntity) player).getCooldowns().addCooldown(stack.getItem(), 22);
                 }
-                target.hurt(DamageSource.mobAttack(player), 7.0F);
-                target.addEffect(new EffectInstance(Effects.POISON, 100, 7));
-                ((PlayerEntity) player).getCooldowns().addCooldown(stack.getItem(), 22);
+
                 Hand hand = player.getUsedItemHand();
                 player.swing(hand);
                 player.stopUsingItem();
             }
 
-            if(player.getDeltaMovement().y > 0.7D) {
+            if(player.getDeltaMovement().y > 0.1D) {
                 player.setDeltaMovement(player.getDeltaMovement().x, player.getDeltaMovement().y * 0.2, player.getDeltaMovement().z);
             }
         }
