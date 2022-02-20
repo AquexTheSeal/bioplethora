2305833bd183e7a49e40371936f541e377edc699
Sajevius <sajeviuscrend@gmail.com>
2022-02-20 15:32:42 +0800

    Nandbric Shortsword improvements + added tooltip

--- src/main/java/io/github/bioplethora/item/weapons/NandbricShortswordItem.java
+++ src/main/java/io/github/bioplethora/item/weapons/NandbricShortswordItem.java
@@ -1,5 +1,8 @@
 package io.github.bioplethora.item.weapons;
 
+import io.github.bioplethora.item.ItemSettings;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileHelper;
@@ -14,11 +17,26 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class NandbricShortswordItem extends SwordItem {
     public NandbricShortswordItem(IItemTier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
         super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        ItemSettings.sacredLevelText(tooltip);
+
+        tooltip.add(new TranslationTextComponent("item.bioplethora.nandbric_shortsword.toxin_rush.skill").withStyle(ItemSettings.SKILL_NAME_COLOR));
+        if(Screen.hasShiftDown() || Screen.hasControlDown()) {
+            tooltip.add(new TranslationTextComponent("item.bioplethora.nandbric_shortsword.toxin_rush.desc").withStyle(ItemSettings.SKILL_DESC_COLOR));
+        }
     }
 
     @Override
@@ -71,7 +89,7 @@ public class NandbricShortswordItem extends SwordItem {
             return !filter.isSpectator() && filter.isPickable();
         }, distance);
 
-        if(result != null) {
+        if(result != null && result.getEntity() != player) {
             LivingEntity target = (LivingEntity)result.getEntity();
 
             double distToTarget = target.distanceToSqr(player.getX(), player.getY(), player.getZ());
@@ -90,11 +108,21 @@ public class NandbricShortswordItem extends SwordItem {
             player.setDeltaMovement(vecX, vecY, vecZ);
 
             if (distToTarget <= attackReachSqr) {
+                World world = player.level;
+                if(!world.isClientSide) {
+                    world.playSound(null, new BlockPos(player.getX(), player.getY(), player.getZ()), SoundEvents.ZOMBIE_INFECT, SoundCategory.PLAYERS, 1, 1);
+                    world.playSound(null, new BlockPos(player.getX(), player.getY(), player.getZ()), SoundEvents.PLAYER_ATTACK_SWEEP, SoundCategory.PLAYERS, 1, 1);
+                }
                 target.hurt(DamageSource.mobAttack(player), 7.0F);
-                ((PlayerEntity) player).getCooldowns().addCooldown(stack.getItem(), 25);
+                target.addEffect(new EffectInstance(Effects.POISON, 100, 7));
+                ((PlayerEntity) player).getCooldowns().addCooldown(stack.getItem(), 22);
                 Hand hand = player.getUsedItemHand();
                 player.swing(hand);
                 player.stopUsingItem();
+            }
+
+            if(player.getDeltaMovement().y > 0.7D) {
+                player.setDeltaMovement(player.getDeltaMovement().x, player.getDeltaMovement().y * 0.2, player.getDeltaMovement().z);
             }
         }
     }
